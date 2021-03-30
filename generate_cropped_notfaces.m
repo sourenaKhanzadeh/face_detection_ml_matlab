a6bac7e079add35988f2abea22eab355a871be42
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-03-29 21:38:03 -0400

    UPDATE: cropped random images 36x36 dims and saved in dir

--- generate_cropped_notfaces.m
+++ generate_cropped_notfaces.m
@@ -2,6 +2,7 @@
 n_have = 0;
 n_want = numel(dir('cropped_training_images_faces/*.jpg'));
 
+
 imageDir = 'images_notfaces';
 imageList = dir(sprintf('%s/*.jpg',imageDir));
 nImages = length(imageList);
@@ -12,7 +13,18 @@ mkdir(new_imageDir);
 dim = 36;
 
 while n_have < n_want
-    
     % generate random 36x36 crops from the non-face images
+    % choose image based on the modulus time
+    % to loop over all images
+    index = mod(n_have, nImages) + 1;
+    image = imread(strcat(imageList(index).folder, "\\",imageList(index).name));
+    image = rgb2gray(image);
     
+    % randomly crop a part of an image
+    win =  randomCropWindow2d(size(image), [dim , dim]);
+    crop =  imcrop(image, win);
+    % save to directory
+    imwrite(crop, strcat(new_imageDir, "\\img_",string(n_have), ".jpg"));
+    % go to next frame
+    n_have = n_have + 1;
 end

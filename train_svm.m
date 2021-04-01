d171eea09065b50fff0bcbed739d988e6c86f582
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-03-31 22:52:19 -0400

    UPDATE: test on validation

--- train_svm.m
+++ train_svm.m
@@ -5,6 +5,10 @@ load('pos_neg_valid_feats.mat');
 feats = cat(1,x_pos_train,x_neg_train);
 labels = cat(1,ones(pos_nImages,1),-1*ones(neg_nImages,1));
 
+valid_feats = cat(1,x_pos_valid,x_neg_valid);
+valid_labels = cat(1,ones(valid_pos_nImages,1),-1*ones(valid_neg_nImages,1));
+
+
 lambda = 0.1;
 [w,b] = vl_svmtrain(feats',labels',lambda);
 
@@ -12,3 +16,9 @@ fprintf('Classifier performance on train data:\n')
 confidences = [x_pos_train; x_neg_train]*w + b;
 
 [tp_rate, fp_rate, tn_rate, fn_rate] =  report_accuracy(confidences, labels);
+
+
+fprintf('Classifier performance on test data:\n')
+valid_confidences = [x_pos_valid; x_neg_valid]*w + b;
+
+[tp_rate_valid, fp_rate_valid, tn_rate_valid, fn_rate_valid] =  report_accuracy(valid_confidences, valid_labels);

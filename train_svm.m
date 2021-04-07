de03d5294b4a0dc445eac156ea59cee22335d1b6
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-07 17:28:31 -0400

    UPDATE: class image face detection

--- train_svm.m
+++ train_svm.m
@@ -1,6 +1,6 @@
 run('../vlfeat-0.9.21/toolbox/vl_setup')
 
 clf = Classifier; 
-[w, b, acc] = clf.train();
+[w, b, acc] = clf.train('pos_neg_feats.mat', 'pos_neg_valid_feats.mat');
 
 save('my_svm.mat', 'w', 'b');

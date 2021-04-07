c220bd25e0ee14e134b35368670bef3b567707c2
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-07 14:50:08 -0400

    UPDATE: Classifer can now train model

--- Classifier.m
+++ Classifier.m
@@ -3,6 +3,7 @@ classdef Classifier
         feat_cellSize = 3;
         feat_n_cell = 12;
         featSize = 31;
+        lambda = 0.0001
     end
     
     methods
@@ -30,7 +31,34 @@ classdef Classifier
             end
         end
         
-        function debug_features(self, im, feat)
+        function [w, b, accs] = train(self)
+            load('pos_neg_feats.mat');
+            load('pos_neg_valid_feats.mat');
+
+            feats = cat(1,x_pos_train,x_neg_train);
+            labels = cat(1,ones(pos_nImages,1),-1*ones(neg_nImages,1));
+
+            valid_feats = cat(1,x_pos_valid,x_neg_valid);
+            valid_labels = cat(1,ones(valid_pos_nImages,1),-1*ones(valid_neg_nImages,1));
+
+
+            [w,b] = vl_svmtrain(feats',labels',self.lambda);
+
+            fprintf('Classifier performance on train data:\n')
+            confidences = [x_pos_train; x_neg_train]*w + b;
+
+            [tp_rate, fp_rate, tn_rate, fn_rate] =  report_accuracy(confidences, labels);
+
+
+            fprintf('Classifier performance on test data:\n')
+            valid_confidences = [x_pos_valid; x_neg_valid]*w + b;
+
+            [tp_rate_valid, fp_rate_valid, tn_rate_valid, fn_rate_valid] =  report_accuracy(valid_confidences, valid_labels);
+            
+            accs = [tp_rate fp_rate tn_rate fn_rate; tp_rate_valid fp_rate_valid tn_rate_valid fn_rate_valid];
+        end
+        
+        function debug_features(~, im, feat)
                 imhog = vl_hog('render', feat);
                 subplot(1,2,1);
                 imshow(im);
